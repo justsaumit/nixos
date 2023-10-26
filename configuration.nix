3c9fe6dc6a5689acf21dfda41e92d3a14af22634
Saumit Dinesan <justsaumit@protonmail.com>
2023-10-27 00:49:20 +0530

    Day 1: Setting envvars, packages and fonts

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -10,66 +10,126 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.device = "nodev";
+  #boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  networking.hostName = "draconix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  # Enable networking
+  networking.networkmanager.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
 
   # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+    enable = true;
+    displayManager.sddm.enable = true;
+  };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
+  users.users.saumit = {
+    isNormalUser = true;
+    description = "Saumit Dinesan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    aria
+    bluez
+    brave
+    dunst
+    firefox
+    foot
+    fzf
+    git
+    gnome.seahorse
+    grim
+    grsync
+    gtk3
+    htop
+    imv
+    kitty
+    killall
+    mpv
+    neofetch
+    neovim
+    pass-wayland
+    pamixer
+    pinentry
+    pfetch
+    ranger
+    rofi
+    slurp
+    tofi
+    vscode
+    waybar
+    wf-recorder
+    wl-clipboard
+    wpaperd
+    xfce.thunar
+  ];
+
+fonts = {
+    fonts  = with pkgs; [
+      # icon fonts
+      material-symbols
+      # normal fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      roboto
+      liberation_ttf
+      maple-mono
+      maple-mono-NF
+      maple-mono-SC-NF
+      # nerdfonts
+      (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
+  ];
+  fontconfig = {
+	enable = true;
+	defaultFonts = {
+		monospace = ["JetBrainsMono Nerd Font Mono"];
+		serif = ["Noto Serif"];
+		sansSerif = ["Noto Sans"];
+	};
+  };
+};
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -78,8 +138,57 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  programs.hyprland = {
+	enable = true;
+	nvidiaPatches = true;
+  };
+  programs.hyprland.xwayland = {
+	hidpi = true;
+	enable = true;
+  };
+  environment.sessionVariables = {
+  # If your cursor becomes invisible
+  WLR_NO_HARDWARE_CURSORS = "1";
+  # Hint electron apps to use wayland
+  NIXOS_OZONE_WL = "1";
+};
+
+  hardware = {
+    # Opengl
+    opengl.enable = true;
+
+    # Most wayland compositors need this
+    nvidia.modesetting.enable = true;
+    bluetooth.enable = true; # enables support for Bluetooth
+
+  };
+
+  # XDG portal
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # rofi keybind
+  #bind = $mainMod, S, exec, rofi -show drun -show-icons
 
   # List services that you want to enable:
+  services.gnome.gnome-keyring.enable = true;
+  services.pcscd.enable = true;
+   programs.gnupg.agent = {
+       enable = true;
+       pinentryFlavor = "curses";
+   };
+
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -90,18 +199,12 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
+  # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
-
